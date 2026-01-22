9415c33bc0165b89f5231780da4c18e1672b366b
Repsackk_unofficial <15867@eans-nt.edu.pl>
2026-01-22 15:26:50 +0100

    calc 1.0

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+import random
+import time
+
+print("Wybierz co chcesz zrobić:")
+print("1. Dodawanie")
+print("2. Odejmowanie")
+print("3. Mnożenie")
+print("4. Dzielenie")
+print("5. Potęgowanie")
+print("6. Pierwiastkowanie")
+print("7. Losowanie liczby z zakresu")
+print("8. Obliczanie silni")
+print("9. Obliczanie logarytmu")
+print("Różniczkowanie i całkowanie")
+print("Optymalizacja")
+print("Statystyka")
+print("Macierze")
